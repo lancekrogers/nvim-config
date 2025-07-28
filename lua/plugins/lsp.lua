ae15da77f0156c96c778ba8b2aa50e926f2b54e1
lancekrogers <lancekrogers@gmail.com>
2025-07-28 13:17:58 -0600

    Fixed lsp config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,29 +1,30 @@
 return {
   "neovim/nvim-lspconfig",
   opts = {
-    setup = {
-      -- disable stock pyright
-      pyright = function()
-        return true
-      end,
-    },
     servers = {
-      pyright = nil, -- nuke it
       basedpyright = {
-        cmd_env = {
-          -- inherit the vars direnv.vim just injected
-          VIRTUAL_ENV = vim.env.VIRTUAL_ENV,
-          PATH = vim.env.PATH,
-          PYTHONPATH = vim.env.PYTHONPATH or "",
-        },
+        mason = false, -- Let Mason handle the installation
         settings = {
           basedpyright = {
-            typeCheckingMode = "basic",
-            pythonVersion = "3.11",
-            diagnosticMode = "workspace",
+            analysis = {
+              typeCheckingMode = "basic",
+              diagnosticMode = "workspace",
+              autoSearchPaths = true,
+              useLibraryCodeForTypes = true,
+            },
           },
         },
       },
+    },
+    setup = {
+      basedpyright = function(_, opts)
+        -- Ensure direnv environment variables are passed
+        opts.cmd_env = {
+          VIRTUAL_ENV = vim.env.VIRTUAL_ENV,
+          PATH = vim.env.PATH,
+          PYTHONPATH = vim.env.PYTHONPATH or "",
+        }
+      end,
     },
   },
 }
